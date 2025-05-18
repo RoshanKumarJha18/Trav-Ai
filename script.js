9706670fe0071d2179245e18086f0d875a736af8
RoshanKumarJha18 <roshanjhajha76@gmail.com>
2025-05-18 22:16:42 +0530

    changed...

--- script.js
+++ script.js
@@ -232,15 +232,3 @@ function canva() {
 canva()
 
 
-function openModal(imageSrc) {
-  const modal = document.getElementById("imageModal");
-  const modalImg = document.getElementById("modalImage");
-  modal.style.display = "block";
-  modalImg.src = imageSrc;
-}
-openModal()
-
-function closeModal() {
-  document.getElementById("imageModal").style.display = "none";
-}
-closeModal()
